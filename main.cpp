6902bc4122fb0633d7ba1ea18f3dc05b874da4fb
AbelJM <abelnav12@gmail.com>
2017-08-18 23:03:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
-#include <iostream>
-#include <string>
+#include <string>//nesecitaremos esta libreria para usar los metodos para la string
+#include <string>//nesecitaremos esta libreria para usar los metodos para la string
 
 using namespace std;
 
-int main(){
-	
-	string nombre,serial;
+int main() {
+	string nombre,serial; //declaramos variable
 	cout<<"ingresa tu nombre: "<<endl;
 	cin>>nombre;
+	
+	// aca haremos el bucle para que ponga nuestro nombre al revez ,como pueden ver restaremos 1 al largo por que es null al final
 	for(int i=nombre.length()-1;i>=0;i--){
 		
-		serial= serial + nombre[i];	
-	}
+		serial = serial + nombre[i]; // guardaremos en la variable serial el resultado
+		
+	}	
 	
-	cout<<serial;
+	cout<<"tu serial es: "<<serial<<"-";
 	
-
-	
-	
-	return 0;
+	// aca haremos otro bucle para que arroje el nombre en su ascii decimal
+	for(int i=0;i<=serial.length()-1;i++){
+		
+    cout<<dec<<(int)serial[i];
+	}	
+	// ahora lo tenemos amigos disculpen por los problemas tecnicos mi pc no esta buena jejeje
+	// nos vemos y saludos de su amigo AbelJM	
 }
